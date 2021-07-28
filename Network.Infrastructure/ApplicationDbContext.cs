44c3c887f553d750be98df093e96db6913d08880
Vladislav <andreevich96@gmail.com>
2021-07-28 13:25:43 +0300

    AddedDefaultData

--- Network.Infrastructure/ApplicationDbContext.cs
+++ Network.Infrastructure/ApplicationDbContext.cs
@@ -20,25 +20,35 @@ namespace Network.Infrastructure
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            var dt = new DateTime();
+            var f = dt.AddDays(28).AddMonths(7).AddYears(1990);
+            var f1 = dt.AddDays(05).AddMonths(05).AddYears(1985);
+            var f2 = dt.AddDays(1).AddMonths(02).AddYears(1955);
+
             modelBuilder.Entity<Birthday>().HasData(new Birthday
             {
+                Id = 100,
                 Note = "Не забыть поздравить хоть в этот раз",
                 Name = "Петров Петр Петрович",
-                DateOfBirth = new DateTime(28, 07, 1990),
+                DateOfBirth = f,
                 Relationship = "Брат"
             },
+            
             new Birthday
             {
+                Id = 101,
                 Note = "Новая заметка",
                 Name = "Петя Александров",
-                DateOfBirth = new DateTime(05, 05, 1985),
+                DateOfBirth = f1,
                 Relationship = "Брат"
             },
+
             new Birthday
             {
+                Id = 102,
                 Note = "Заметка 3",
                 Name = "Алексей Иванов Александрович",
-                DateOfBirth = new DateTime(01, 15, 1955),
+                DateOfBirth = f2,
                 Relationship = "Друг"
             });
         }
---
+++ Network.Infrastructure/Migrations/20210728102449_AddedDefaultData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Network.Infrastructure.Migrations
+{
+    public partial class AddedDefaultData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Birthdays",
+                columns: new[] { "Id", "DateOfBirth", "Name", "Note", "Photo", "Relationship" },
+                values: new object[] { 100, new DateTime(1991, 8, 29, 0, 0, 0, 0, DateTimeKind.Unspecified), "Петров Петр Петрович", "Не забыть поздравить хоть в этот раз", null, "Брат" });
+
+            migrationBuilder.InsertData(
+                table: "Birthdays",
+                columns: new[] { "Id", "DateOfBirth", "Name", "Note", "Photo", "Relationship" },
+                values: new object[] { 101, new DateTime(1986, 6, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), "Петя Александров", "Новая заметка", null, "Брат" });
+
+            migrationBuilder.InsertData(
+                table: "Birthdays",
+                columns: new[] { "Id", "DateOfBirth", "Name", "Note", "Photo", "Relationship" },
+                values: new object[] { 102, new DateTime(1956, 3, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "Алексей Иванов Александрович", "Заметка 3", null, "Друг" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Birthdays",
+                keyColumn: "Id",
+                keyValue: 100);
+
+            migrationBuilder.DeleteData(
+                table: "Birthdays",
+                keyColumn: "Id",
+                keyValue: 101);
+
+            migrationBuilder.DeleteData(
+                table: "Birthdays",
+                keyColumn: "Id",
+                keyValue: 102);
+        }
+    }
+}
