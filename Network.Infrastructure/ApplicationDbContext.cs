c23ec37ed85d8b272a66593c9fcc843d1e371a6b
Vladislav <andreevich96@gmail.com>
2021-07-28 13:10:38 +0300

    final

--- Network.Infrastructure/ApplicationDbContext.cs
+++ Network.Infrastructure/ApplicationDbContext.cs
@@ -17,6 +17,30 @@ namespace Network.Infrastructure
             //Database.EnsureDeleted();
             //Database.EnsureCreated();
         }
-      
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Birthday>().HasData(new Birthday
+            {
+                Note = "Не забыть поздравить хоть в этот раз",
+                Name = "Петров Петр Петрович",
+                DateOfBirth = new DateTime(28, 07, 1990),
+                Relationship = "Брат"
+            },
+            new Birthday
+            {
+                Note = "Новая заметка",
+                Name = "Петя Александров",
+                DateOfBirth = new DateTime(05, 05, 1985),
+                Relationship = "Брат"
+            },
+            new Birthday
+            {
+                Note = "Заметка 3",
+                Name = "Алексей Иванов Александрович",
+                DateOfBirth = new DateTime(01, 15, 1955),
+                Relationship = "Друг"
+            });
+        }
     }
 }
